2867490ac8307c0de7fa8b6ad1676879036bae96
meguncasey <meganleo@live.co.uk>
2024-03-25 15:45:32 +0000

    Add files via upload

---
+++ Fahrenheit to celsius converter/Fahrenheit to celsius converter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Fahrenheit_to_celsius_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            this.lblEquiv = new System.Windows.Forms.Label();
+            this.lblCelsius = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Location = new System.Drawing.Point(47, 76);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(212, 20);
+            this.txtFahrenheit.TabIndex = 0;
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Location = new System.Drawing.Point(64, 47);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(178, 13);
+            this.lblFahrenheit.TabIndex = 1;
+            this.lblFahrenheit.Text = "Enter Fahrenheit Temperature here: ";
+            // 
+            // lblEquiv
+            // 
+            this.lblEquiv.AutoSize = true;
+            this.lblEquiv.Location = new System.Drawing.Point(74, 134);
+            this.lblEquiv.Name = "lblEquiv";
+            this.lblEquiv.Size = new System.Drawing.Size(159, 13);
+            this.lblEquiv.TabIndex = 2;
+            this.lblEquiv.Text = "Equivalent Celsius Temperature ";
+            this.lblEquiv.Click += new System.EventHandler(this.lblCelsius_Click);
+            // 
+            // lblCelsius
+            // 
+            this.lblCelsius.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCelsius.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCelsius.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblCelsius.Location = new System.Drawing.Point(38, 165);
+            this.lblCelsius.Name = "lblCelsius";
+            this.lblCelsius.Size = new System.Drawing.Size(231, 29);
+            this.lblCelsius.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(12, 303);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(76, 33);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(110, 304);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(83, 32);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(220, 309);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 26);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 357);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblCelsius);
+            this.Controls.Add(this.lblEquiv);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Name = "Form1";
+            this.Text = "Fahrenheit to Celsius Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.Label lblFahrenheit;
+        private System.Windows.Forms.Label lblEquiv;
+        private System.Windows.Forms.Label lblCelsius;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Fahrenheit to celsius converter/Fahrenheit to celsius converter/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fahrenheit_to_celsius_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCelsius_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double C;
+            double F;
+            F = double.Parse(txtFahrenheit.Text);
+            C = 5.0 / 9.0 * (F - 32);
+            lblCelsius.Text = C.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblCelsius.Text = "";
+            txtFahrenheit.Text = "";
+            txtFahrenheit.Focus(); 
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
