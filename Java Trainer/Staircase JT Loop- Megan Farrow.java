e2f3d85df03dbb582cc7cd8a4f35fcd7063ffc27
meguncasey <meganleo@live.co.uk>
2024-03-25 15:56:08 +0000

    Add files via upload

---
+++ Java Trainer/Staircase JT Loop- Megan Farrow.java
@@ -0,0 +1,27 @@
+int i;
+createpieeater();
+
+turnright();
+turnright();
+
+walk();
+walk();
+walk();
+walk();
+walk();
+walk();
+
+pendown();
+
+turnleft();
+turnleft();
+
+for (i = 1 ; i <= 5 ; i++)
+   {
+   walk();
+   turnleft();
+   turnleft();
+   walk();
+   turnright();
+   turnright();
+   }
---
+++ Java Trainer/boundry walk - Megan Farrow.java
@@ -0,0 +1,35 @@
+int i;
+createpieeater();
+
+for (i=0 ; i<7 ; i++)
+   {
+   pendown();
+   walk();
+   }
+
+turnright();
+turnright();
+
+while (clearahead)
+   {
+   pendown();
+   walk();
+   }
+
+turnright();
+turnright();
+
+for (i=0 ; i<7 ; i++)
+   {
+   pendown();
+   walk();
+   }
+
+turnright();
+turnright();
+
+while (clearahead)
+   {
+   pendown();
+   walk();
+   }
